f321a5d81b45c019caf8ad7b5a2140346f2d9b7b
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-03 18:34:26 +0200

    table transaction added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,14 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
 import SideBar from '../Layouts/SideBar'
+import TransactionTable from '../Components/TransactionTable'
 
 const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
    <Route path='/'  element={<SideBar />} />
+   <Route path='/history' element={<TransactionTable/>} />
   </Routes>
   </BrowserRouter>
   )
