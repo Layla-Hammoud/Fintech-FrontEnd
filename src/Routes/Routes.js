1e779dc819768daadcaaecdfceef62c4ef2786bf
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-03 23:45:52 +0200

    finished CardBalance component

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,14 +1,15 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
 import SideBar from '../Layouts/SideBar'
-import TransactionTable from '../Components/TransactionTable'
+// import TransactionTable from '../Components/TransactionTable'
+import CardBalance from '../Components/CardBalance'
 
 const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
    <Route path='/'  element={<SideBar />} />
-   <Route path='/history' element={<TransactionTable/>} />
+   <Route path='/history' element={<CardBalance title='USD Balance' amount='128,320' unit='$' borderColor='#0F5533' backGroundColor='green'/>} />
   </Routes>
   </BrowserRouter>
   )
