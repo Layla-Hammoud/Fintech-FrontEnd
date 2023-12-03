591b672cb72982849ee95b7a9d16357ad5bd1de5
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-04 01:21:51 +0200

    component without styling for promotion Grid

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,10 +1,12 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
+import DataGridPromotions from '../Components/DataGridPromotions'
 
 const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
+    <Route path='/' element={<DataGridPromotions />}></Route>
   </Routes>
   </BrowserRouter>
   )
