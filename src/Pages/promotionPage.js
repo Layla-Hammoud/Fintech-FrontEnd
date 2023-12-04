25f406123f5da2f5df73ef558b54c0c508113e49
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-12-04 10:42:22 +0200

    Added promotions cards

---
+++ src/Pages/promotionPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PromotionLayout from '../Layouts/promotions/promotionCard.js';
+// import PrimarySearchAppBar from '../Layouts/promotions/promotionLayout.js'
+function PromotionPage() {
+  return (
+    <>
+    
+    <PromotionLayout/>
+   
+    </>
+    
+  )
+}
+export default PromotionPage;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,10 +1,12 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
+import PromotionPage from '../Pages/promotionPage'
 
 const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
+    <Route path='/promotions' element={<PromotionPage/>}></Route>
   </Routes>
   </BrowserRouter>
   )
