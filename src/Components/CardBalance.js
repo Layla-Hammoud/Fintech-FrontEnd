b65ff35b175a5c7b3f6859e9e16b8b40d4894807
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-04 12:30:31 +0200

    adding Card component

---
+++ src/Components/CardBalance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Paper, Typography, Box } from '@mui/material'
+const CardBalance = ({ title, amount, unit, backGroundColor, borderColor }) => {
+  return (
+    <Paper variant='outlined' sx={{ width: 320, height: 170, m: 10, pt: 0.5, pb: 0.5, borderRadius: '12px', border: '1px solid #E6E9EE', backgroundColor: { backGroundColor } }}>
+      <Box sx={{ borderLeft: '8px solid', height: '100%', borderRadius: 2, borderLeftColor: { borderColor } }}>
+        <Typography variant='h6' sx={{ p: 2, ml: 2, color: '#AFAFAF' }} >{title}</Typography>
+        <Typography variant='body1' sx={{ p: 2, ml: 2, fontWeight: '600', fontSize: '20px' }}>{unit} {amount}</Typography>
+      </Box>
+    </Paper>
+  )
+}
+
+export default CardBalance
