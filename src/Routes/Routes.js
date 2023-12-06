3e82df548db2e0bb7af769281734ba8657e378fa
Layla Hammoud <laylahammoud63@gmail.com>
2023-12-06 12:11:37 +0200

    make the route as befor

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,5 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
-import StatisticCard from '../Components/StatisticCard/StatisticCard'
 import SideBar from '../Layouts/SideBar'
 import TransactionTable from '../Components/TransactionTable/TransactionTable'
 import PromotionPage from '../Pages/promotionPage'
@@ -11,7 +10,7 @@ const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
-      <Route path='/' element={<StatisticCard/>} >
+      <Route path='/' element={<SideBar/>} >
    <Route path='history' element={<TransactionTable/>} />
    <Route path='/promotions' element={<PromotionPage/>}></Route>
    </Route>
