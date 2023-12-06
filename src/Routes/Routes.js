201b4a1252f26ee8a7d56a73b68ab37b6662f088
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-06 22:12:57 +0200

    fixing routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,22 +1,22 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import React from 'react'
 
-// import SideBar from '../Layouts/SideBar'
-// import TransactionTable from '../Components/TransactionTable/TransactionTable'
-// import PromotionPage from '../Pages/promotionPage'
-// import SavingSection from '../Components/SavingModal/SavingSection.js'
+import SideBar from '../Layouts/SideBar/SideBar'
+import TransactionTable from '../Components/TransactionTable/TransactionTable'
+import PromotionPage from '../Pages/promotionPage'
+import SavingSection from '../Components/SavingModal/SavingSection.js'
 import SavingPage from '../Pages/Login.js'
 const AppRoutes = () => {
   return (
   <BrowserRouter>
   <Routes>
-    {/* <Route path='/' element={<SideBar/>} > */}
-   {/* <Route path='history' element={<TransactionTable/>} /> */}
-   {/* <Route path='/promotions' element={<PromotionPage/>}></Route> */}
-   {/* <Route path='/EditModal' element={<SavingPage/>}></Route> */}
+    <Route path='/' element={<SideBar/>} >
+   <Route path='history' element={<TransactionTable/>} />
+   <Route path='/promotions' element={<PromotionPage/>}></Route>
+   <Route path='/EditModal' element={<SavingPage/>}></Route>
    <Route path='/SavingSection' element={<SavingPage/>}></Route>
 
-   {/* </Route> */}
+   </Route>
   </Routes>
   </BrowserRouter>
   )
