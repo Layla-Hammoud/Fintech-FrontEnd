44cc4484ec76a50fda085a4a680f7e6f45da24a3
Layla Hammoud <laylahammoud63@gmail.com>
2023-12-08 11:04:17 +0200

    add authcontext

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,108 +1,139 @@
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import "./auth.css";
 import { Link } from "react-router-dom";
+import useApi from "../../hooks/useApi";
+import { toast } from "react-toastify";
 import { useState } from "react";
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const { apiCall } = useApi();
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })}
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setFormData({
-        email: '',
-        password: '',
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!formData.email || !formData.password) {
+      toast.error("Please insert email or password");
+      setLoading(false);
+      return;
+    }
+    setFormData({
+      email: "",
+      password: "",
+    });
+
+    try {
+       await apiCall({
+        url: "/api/users/login",
+        method: "post",
+        data: {
+          email: formData.email,
+          password: formData.password,
+        },
       });
-    };
-  
+
+      // await fetchUserData()
+      // toast.success("Logged in Successfully!")
+      // setLoading(false);
+      // navigate('/')
+    } catch (error) {
+      console.log(error);
+
+      setLoading(false);
+    }
+  };
+
   return (
     <>
+      <Box
+        sx={{
+          flex: "1 1 auto",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <Box
           sx={{
-            flex: "1 1 auto",
-            alignItems: "center",
-            display: "flex",
-            justifyContent: "center",
+            maxWidth: 550,
+            px: 3,
+            py: "100px",
+            width: "100%",
           }}
         >
-          <Box
-            sx={{
-              maxWidth: 550,
-              px: 3,
-              py: "100px",
-              width: "100%",
-            }}
-          >
-            <div>
-              <Stack spacing={1} sx={{ mb: 3 }}>
-                <Typography variant="h4">Log In</Typography>
-                <Typography variant="body2">
-                  Don't have an account? <Link to="/sign-up">Sign up</Link>
-                </Typography>
-              </Stack>
-              <form onSubmit={handleSubmit}>
-                <Stack spacing={3}>
-                  <TextField
-                    fullWidth
-                    label="Email Address"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    required
-                    sx={{
-                      "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
-                        {
-                          borderColor: "#119c59",
-                        },
-                      "& .MuiInputLabel-root.Mui-focused": {
-                        color: "#119c59",
-                      },
-                    }}
-                  />
-                  <TextField
-                    fullWidth
-                    label="Password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    type="password"
-                    required
-                    sx={{
-                      "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
-                        {
-                          borderColor: "#119c59",
-                        },
-                      "& .MuiInputLabel-root.Mui-focused": {
-                        color: "#119c59",
-                      },
-                    }}
-                  />
-                </Stack>
-                <Button
+          <div>
+            <Stack spacing={1} sx={{ mb: 3 }}>
+              <Typography variant="h4">Log In</Typography>
+              <Typography variant="body2">
+                Don't have an account? <Link to="/sign-up">Sign up</Link>
+              </Typography>
+            </Stack>
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={3}>
+                <TextField
                   fullWidth
-                  size="large"
+                  label="Email Address"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
                   sx={{
-                    mt: 3,
-                    backgroundColor: "#119c59",
-                    "&:hover": {
-                      backgroundColor: "#2C6E49",
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                      {
+                        borderColor: "#119c59",
+                      },
+                    "& .MuiInputLabel-root.Mui-focused": {
+                      color: "#119c59",
                     },
                   }}
-                  type="submit"
-                  variant="contained"
-                >
-                  Continue
-                </Button>
-              </form>
-            </div>
-          </Box>
+                />
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  type="password"
+                  required
+                  sx={{
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                      {
+                        borderColor: "#119c59",
+                      },
+                    "& .MuiInputLabel-root.Mui-focused": {
+                      color: "#119c59",
+                    },
+                  }}
+                />
+              </Stack>
+              <Button
+                fullWidth
+                size="large"
+                sx={{
+                  mt: 3,
+                  backgroundColor: "#119c59",
+                  "&:hover": {
+                    backgroundColor: "#2C6E49",
+                  },
+                }}
+                type="submit"
+                variant="contained"
+              >
+                Continue
+              </Button>
+            </form>
+          </div>
         </Box>
+      </Box>
     </>
   );
 };
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,31 +1,29 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
-import SideBar from '../Layouts/SideBar/SideBar'
-import TransactionTable from '../Components/TransactionTable/TransactionTable'
-import PromotionPage from '../Pages/promotionPage'
-import SavingSection from '../Components/SavingModal/SavingSection.js'
-import SavingPage from '../Pages/Login.js'
-import GridUser from '../Components/GridUser/GridUser'
-import GridTransaction from '../Components/GridTransaction/GridTransaction'
-import GridPromotion from '../Components/GridPromotion/GridPromotion'
-
+import SideBar from "../Layouts/SideBar/SideBar";
+import TransactionTable from "../Components/TransactionTable/TransactionTable";
+import PromotionPage from "../Pages/promotionPage";
+import SavingSection from "../Components/SavingModal/SavingSection.js";
+import SavingPage from "../Pages/Login.js";
+import GridUser from "../Components/GridUser/GridUser";
+import GridTransaction from "../Components/GridTransaction/GridTransaction";
+import GridPromotion from "../Components/GridPromotion/GridPromotion";
+import LoginPage from "../Pages/Login/LoginPage.js"
 const AppRoutes = () => {
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<SideBar/>} >
-   <Route path='users' element={<GridUser/>} />
-   <Route path='history' element={<TransactionTable/>} />
-   <Route path='transaction' element={<GridPromotion/>}/>
-   <Route path='/promotions' element={<PromotionPage/>}></Route>
-   <Route path='/EditModal' element={<SavingPage/>}></Route>
-   <Route path='/SavingSection' element={<SavingPage/>}></Route>
-
-   </Route>
-  </Routes>
-  </BrowserRouter>
-  )
-}
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/" element={<SideBar />}>
+          <Route path="users" element={<GridUser />} />
+          <Route path="history" element={<TransactionTable />} />
+          <Route path="transaction" element={<GridPromotion />} />
+          <Route path="/promotions" element={<PromotionPage />}></Route>
+          <Route path="/EditModal" element={<SavingPage />}></Route>
+          <Route path="/SavingSection" element={<SavingPage />}></Route>
+        </Route>
+      </Routes>
+  );
+};
 
 export default AppRoutes;
